c7d329aa17f72f62daae75eb10264bbd34ed2b6d
Fernando <fernando-perugachi@hotmail.com>
2022-06-02 18:53:51 -0500

    Update changes in all files

--- src/components/pokedex/Pokedex.js
+++ src/components/pokedex/Pokedex.js
@@ -4,6 +4,8 @@ import { useSelector } from 'react-redux';
 import axios from 'axios';
 import PokemonCard from '../pokemonCard/PokemonCard';
 import { useNavigate } from 'react-router-dom';
+import salute from '../../image/salute.gif'
+// import { v4 as uuidv4 } from "uuid";
 
 const Pokedex = () => {
 
@@ -13,20 +15,19 @@ const Pokedex = () => {
     const [types, setTypes] = useState([])
     const [searchPokemons, setSearchPokemons] = useState("")
 
-    // const [page, setPage]
-
+    
     const navigate = useNavigate();
-
+    
     useEffect(()=>{
-        axios.get("https://pokeapi.co/api/v2/pokemon/")
+        axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1126")
         .then(res=> setPokemons(res.data.results))
 
         axios.get("https://pokeapi.co/api/v2/type/")
         .then(res=> setTypes(res.data.results))        
     },[])
-
+    
     const pokemonsNames = pokemons.map(pokemon=> pokemon.name)
-
+    
     const searchPoke = ()=>{
         if (pokemonsNames.includes(searchPokemons)) {
             navigate(`/pokedex/${searchPokemons}`)
@@ -34,18 +35,28 @@ const Pokedex = () => {
             alert("pokemon don't exist")
         }
     }
-
+    
     const filterType = (e)=>{
         axios.get(e.target.value)
         .then(res => setPokemons(res.data.pokemon))
     }
+    // estado para la paginacion
+    const [page, setPage] = useState(1)
+    
+    const numPokemons = 20;
+    const lastIndex = numPokemons * page;
+    const firstIndex = lastIndex - numPokemons;
+    
+    const pokemonPaginated = pokemons.slice(firstIndex, lastIndex);
 
-    // console.log(pokemons);
-    // console.log(types);
-    // console.log(searchPokemons);
+    const lastPage = Math.ceil(pokemons.length / numPokemons);
 
-    const pokemonPaginated = pokemons.slice(0, 24);
+// numeros de paginacion aun sin funcionar
+    // const numberPagination = []
 
+    // for (let i = 1; i <= lastPage; i++) {
+    //     numberPagination.push(i);
+    // }
 
     return (
         <div className='pokedex'>
@@ -57,7 +68,6 @@ const Pokedex = () => {
                     <div className='circle-short green'></div>
                 </div>
                 <div className='content-bar'>
-                    <p>Welcome {user}</p>
                     <form className='form form-poke' action="" onChange={e => e.preventDefault()}>
                         <button className='btn-ball' onClick={searchPoke}>
                             <i className="fa-solid fa-magnifying-glass"></i>
@@ -70,11 +80,10 @@ const Pokedex = () => {
                             onChange={e=> setSearchPokemons(e.target.value.toLowerCase())}
                         />
                     </form>
-                    <select onChange={filterType} name="" id="">
-                        <option value="">All</option>
+                    <select className='select' onChange={filterType} name="" id="">
                         {
                             types.map(type=>(
-                                <option key={type.url} value={type.url}>
+                                <option className='option' key={type.url} value={type.url}>
                                     {type.name}
                                 </option>
                             ))
@@ -82,11 +91,38 @@ const Pokedex = () => {
                     </select>
                 </div>
             </section>
+            <div className='user'>
+                <img className='salute' src={salute} alt="" />
+                <p>Welcome {user}</p>
+            </div>
             <section className='poke-content'>
+                <div className='btn-content'>
+                    <button 
+                        className='btn-pagination' 
+                        onClick={()=> setPage(page-1)} 
+                        disabled={page === 1}
+                    >
+                        <i className="fa-solid fa-circle-chevron-left"></i>
+                    </button>
+                    {/* esta implementacion aun falta */}
+                        {/* {numberPagination.map(numberPag=> (
+                            <button key={uuidv4()} onClick={()=> setPage(numberPag)}>{numberPag}</button>
+                        ))} */}
+                    <button 
+                        className='btn-pagination' 
+                        onClick={()=> setPage(page+1)} 
+                        disabled={page === lastPage}
+                    >
+                        <i className="fa-solid fa-circle-chevron-right"></i>
+                    </button>
+                </div>
                 <div className='content-cards'>
                 {
                     pokemonPaginated.map(pokemon=>(
-                        <PokemonCard key={pokemon.url} pokeUrl={pokemon.url}/>
+                        <PokemonCard 
+                            key={pokemon.url !== undefined ? pokemon.url : pokemon.pokemon.url} 
+                            pokeUrl={pokemon.url !== undefined ? pokemon.url : pokemon.pokemon.url}
+                        />
                     ))
                 }
                 </div>
--- src/components/pokemonDetail/PokemonDetail.js
+++ src/components/pokemonDetail/PokemonDetail.js
@@ -21,41 +21,42 @@ const PokemonDetail = () => {
             <section className='detail-img'>
                 <img src={pokemonDetail.sprites?.front_default} alt="" />
                 <img src={pokemonDetail.sprites?.back_default} alt="" />
-                <h2 className='info-title'>{pokemonDetail.name}</h2>
             </section>
-            <section className='detail-info'>
-                <div className='info-type'>
-                    {
-                        pokemonDetail.types?.map(type=>(
-                            <span className='type' key={type.slot}>{type.type.name}</span>
-                        ))
-                    }
-                </div>
-                <ul className='info-w_h'>
-                    <li>
-                        <h4>{pokemonDetail.weight}</h4>
-                        <span>Weight</span>
-                    </li>
-                    <li>
-                        <h4>{pokemonDetail.height}</h4>
-                        <span>Height</span>
-                    </li>
-                </ul>
-            </section>
-            <section className='detail-stats'>
-                <h2>Statics</h2>
-                <ul className='stats-list'>
-                            {/* <li key={stat.stat.url}>{stat.stat.name} {stat.base_stat}</li> */}
-                    {
-                        pokemonDetail.stats?.map(stat=>(
-                            <li key={stat.stat.url}>
-                                <label htmlFor="pokerbar">{stat.stat.name} </label>
-                                <progress id="pokebar" max="100" value={stat.base_stat}>{stat.base_stat}</progress>
-                            </li>
-                            
-                        ))
-                    }
-                </ul>
+            <section className='content-info'>
+                <article className='detail-info'>
+                    <h2 className='info-title'>{pokemonDetail.name}</h2>
+                    <div className='info-type'>
+                        {
+                            pokemonDetail.types?.map(type=>(
+                                <span className='type' key={type.slot}>{type.type.name}</span>
+                            ))
+                        }
+                    </div>
+                    <ul className='info-w_h'>
+                        <li>
+                            <h4>{pokemonDetail.weight}</h4>
+                            <span>Weight</span>
+                        </li>
+                        <li>
+                            <h4>{pokemonDetail.height}</h4>
+                            <span>Height</span>
+                        </li>
+                    </ul>
+                </article>
+                <article className='detail-stats'>
+                    <h2>Statics</h2>
+                    <ul className='stats-list'>
+                        {
+                            pokemonDetail.stats?.map(stat=>(
+                                <li key={stat.stat.url}>
+                                    <label htmlFor="pokerbar">{stat.stat.name} </label>
+                                    <progress id="pokebar" max="100" value={stat.base_stat} className='prog'>{stat.base_stat}</progress>
+                                </li>
+                                
+                            ))
+                        }
+                    </ul>
+                </article>
             </section>
         </div>
     );
